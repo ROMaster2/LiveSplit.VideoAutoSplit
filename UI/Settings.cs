2ee32209121aa4d27ec721adb9e94da806814422
ROMaster2 <ddpoulton+github@gmail.com>
2018-11-27 15:34:58 -0800

    Borrow NewTreeView from ScriptableAutoSplit This won't conflict, will it?

--- UI/Settings.cs
+++ UI/Settings.cs
@@ -17,4 +17,34 @@ namespace LiveSplit.VAS.UI
             InitializeComponent();
         }
     }
+
+    /// <summary>
+    /// TreeView with fixed double-clicking on checkboxes.
+    /// </summary>
+    ///
+    /// See also:
+    /// http://stackoverflow.com/questions/17356976/treeview-with-checkboxes-not-processing-clicks-correctly
+    /// http://stackoverflow.com/questions/14647216/c-sharp-treeview-ignore-double-click-only-at-checkbox
+    class NewTreeView : TreeView
+    {
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x203) // identified double click
+            {
+                var local_pos = PointToClient(Cursor.Position);
+                var hit_test_info = HitTest(local_pos);
+
+                if (hit_test_info.Location == TreeViewHitTestLocations.StateImage)
+                {
+                    m.Msg = 0x201; // if checkbox was clicked, turn into single click
+                }
+
+                base.WndProc(ref m);
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+    }
 }
