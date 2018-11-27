5c9412bf5c157776b6bd9e6e0d44c3d4ff0e3836
ROMaster2 <ddpoulton+github@gmail.com>
2018-11-27 14:42:05 -0800

    Sort dependencies for Component Settings

--- UI/NewComponentSettings.cs
+++ UI/NewComponentSettings.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Accord.Video.DirectShow;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+using LiveSplit.VAS.Models;
+using LiveSplit.VAS.UI;
+using LiveSplit.VAS.VASL;
 
 namespace LiveSplit.VAS.UI
 {
