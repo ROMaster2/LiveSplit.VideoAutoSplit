9948417d7ec3016844e0b6bcc857032cf9c5edc6
ROMaster2 <ddpoulton+github@gmail.com>
2018-12-22 20:26:07 -0800

    Update version Beta tiem

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LiveSplit.VideoAutoSplit")]
-[assembly: AssemblyDescription("Video Auto Splitter for LiveSplit.")]
+[assembly: AssemblyDescription("Video Auto Splitter for LiveSplit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LiveSplit.VideoAutoSplit")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
