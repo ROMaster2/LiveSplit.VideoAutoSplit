931d3603a1974b51c1bef7c074122cce8dd35d11
ROMaster2 <ddpoulton+github@gmail.com>
2020-05-14 17:09:27 -0700

    Update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LiveSplit.VideoAutoSplit")]
-[assembly: AssemblyCopyright("Copyright © ROMaster2 2018")]
+[assembly: AssemblyCopyright("Copyright © ROMaster2 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2.2")]
-[assembly: AssemblyFileVersion("0.5.2.2")]
+[assembly: AssemblyVersion("0.5.2.3")]
+[assembly: AssemblyFileVersion("0.5.2.3")]
